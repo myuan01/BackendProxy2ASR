4e5dfe101e8e268c7cde0ccc28a688e349094313
mayuan1 <43050290+mayuan1@users.noreply.github.com>
2021-01-29 16:28:50 +0800

    Initial commit

---
+++ BackendProxy2ASR/ProxyServer.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Fleck;
+using Newtonsoft.Json;
+
+using Serilog;
+using database_and_log;
+
+
+namespace BackendProxy2ASR
+{
+    class AnswerPlusSessionID
+    {
+        public string right_text { get; set; }
+        public string session_id { get; set; }
+        public int sequence_id { get; set; }
+    }
+
+    class ProxyASR
+    {
+        private readonly int m_proxyPort;
+        private readonly String m_asrIP;
+        private readonly int m_asrPort;
+        private readonly int m_sampleRate;
+
+        private List<IWebSocketConnection> m_allSockets;
+        private Dictionary<String, IWebSocketConnection> m_sessionID2sock;
+        private Dictionary<IWebSocketConnection, String> m_sock2sessionID;
+
+        private CommASR m_commASR = null;
+        //private DatabaseHelper dbhelper = null;
+        private Dictionary<String, SessionHelper> m_sessionID2Helper;
+
+        private DatabaseHelper databaseHelper = new DatabaseHelper("./config.json");
+
+        //--------------------------------------------------------------------->
+        // C'TOR: initialize member variables
+        //--------------------------------------------------------------------->
+        public ProxyASR(int proxyPort, String asrIP, int asrPort, int sampleRate)
+        {
+            m_proxyPort = proxyPort;
+            m_asrIP = asrIP;
+            m_asrPort = asrPort;
+            m_sampleRate = sampleRate;
+            m_allSockets = new List<IWebSocketConnection>();
+            m_sessionID2sock = new Dictionary<String, IWebSocketConnection>();
+            m_sock2sessionID = new Dictionary<IWebSocketConnection, String>();
+            m_sessionID2Helper = new Dictionary<String, SessionHelper>();
+
+            m_commASR = CommASR.Create(asrIP, asrPort, sampleRate, m_sessionID2sock, m_sessionID2Helper);
+
+            //dbhelper = new DatabaseHelper("./config.json");
+            
+
+        }
+
+
+        public List<IWebSocketConnection> allSockets
+        {
+            get { return m_allSockets; }
+        }
+
+
+        public void Start()
+        {
+            FleckLog.Level = LogLevel.Error;
+            var server = new WebSocketServer("ws://0.0.0.0:" + m_proxyPort);
+
+            // connect to database
+            //bool connectionResult = dbhelper.Open();
+            //Console.WriteLine("Opening connection success? : " + connectionResult.ToString());
+
+            Console.WriteLine("port: " + m_proxyPort + "  samplerate: " + m_sampleRate);
+
+            server.Start(socket =>
+                {
+                    socket.OnOpen = () =>
+                    {
+                        OnConnect(socket);
+                    };
+
+                    socket.OnClose = () =>
+                    {
+                        OnDisconnect(socket);
+                    };
+
+                    socket.OnMessage = message =>
+                    {
+                        OnMessage(socket, message);
+                    };
+
+                    socket.OnBinary = data =>
+                    {
+                        OnBinaryData(socket, data);
+                    };
+                }
+            );
+        }
+
+        //--------------------------------------------------------------------->
+        // Handle new websocket connect
+        //--------------------------------------------------------------------->
+        private void OnConnect(IWebSocketConnection sock)
+        {
+            Console.WriteLine("WS Connect...");
+            var session = new SessionHelper();
+            sock.Send("0{\"session_id\":" + session.m_sessionID + "}");
+            m_sessionID2Helper[session.m_sessionID] = session;
+            m_allSockets.Add(sock);
+        }
+
+        //--------------------------------------------------------------------->
+        // Handle websocket disconnect
+        //--------------------------------------------------------------------->
+        private void OnDisconnect(IWebSocketConnection sock)
+        {
+            Console.WriteLine("WS Disconnect...");
+            // Disconnect from ASR engine
+            var session_id = m_sock2sessionID[sock];
+            m_commASR.DisconnectASR(session_id);
+            m_allSockets.Remove(sock);
+        }
+
+        //--------------------------------------------------------------------->
+        // Handle websocket text message
+        //--------------------------------------------------------------------->
+        private void OnMessage(IWebSocketConnection sock, String msg)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(msg);
+
+            sock.Send("Echo: " + msg);
+
+            if (msg.Contains("right_text")==false || msg.Contains("session_id")==false || msg.Contains("sequence_id") == false)
+            {
+                Console.WriteLine("message missing 'right_text' OR 'session_id' OR 'sequence_id' .... ignored ....");
+                return;
+            }
+
+            AnswerPlusSessionID aps = JsonConvert.DeserializeObject<AnswerPlusSessionID>(msg);
+            Console.WriteLine(aps.right_text);
+            Console.WriteLine(aps.session_id);
+            Console.WriteLine(aps.sequence_id);
+
+            if (m_sock2sessionID.ContainsKey(sock)==false)
+            {
+                //------------------------------------------------------------->
+                // it is a new session from client:
+                //      (1) create websocket connection to ASR Engine
+                //------------------------------------------------------------->
+                m_sock2sessionID[sock] = aps.session_id;
+                m_sessionID2sock[aps.session_id] = sock;
+
+                Console.WriteLine("map sock -> sessionID: " + aps.session_id);
+                Console.WriteLine("map sessionID " + aps.session_id + " -> sock");
+
+                m_commASR.ConnectASR(aps.session_id);
+            }
+
+            //----------------------------------------------------------------->
+            // Store session and sequence information
+            //----------------------------------------------------------------->
+
+            var session = m_sessionID2Helper[aps.session_id];
+            if(session.m_sequence2inputword.ContainsKey(aps.sequence_id) == false)
+            {
+                session.m_sequence2inputword[aps.sequence_id] = aps.right_text;
+                session.m_sequenceQueue.Enqueue(aps.sequence_id);
+                session.m_sequenceStartTime[aps.sequence_id] = DateTime.UtcNow;
+            }
+
+            //----------------------------------------------------------------->
+            // Insert recrod into database
+            //----------------------------------------------------------------->
+            bool connectionResult = databaseHelper.Open();
+            Console.WriteLine("Opening connection success? : " + connectionResult.ToString());
+
+
+            // update asr_audio_stream_prediction
+            databaseHelper.InsertAudioStreamPrediction(
+                user_id: 1,
+                device_id: "device1",
+                app_id: 1,
+                session_id: aps.session_id,
+                seq_id: aps.sequence_id,
+                input_audio: Encoding.ASCII.GetBytes("new sequence start"),
+                pred_timestamp: DateTime.UtcNow,
+                input_word: aps.right_text,
+                return_text: "");
+
+            databaseHelper.InsertAudioStreamInfo(
+                user_id: 1,
+                device_id: "device1",
+                app_id: 1,
+                session_id: aps.session_id,
+                seq_id: aps.sequence_id,
+                proc_start_time: DateTime.UtcNow,
+                proc_end_time: DateTime.UtcNow,
+                stream_duration: 0);
+
+                // Read from db
+                //databaseHelper.ReadAudioStream(test_session_id, test_seq_id, outputFilePath);
+
+        }
+
+        //--------------------------------------------------------------------->
+        // Handle websocket text message
+        //--------------------------------------------------------------------->
+        private void OnBinaryData(IWebSocketConnection sock, byte[] data)
+        {
+            //Console.WriteLine("OnBinaryData: " + data.Length + " bytes");
+            var sessionID = m_sock2sessionID[sock];
+            m_commASR.SendBinaryData(sessionID, data);
+
+            var session = m_sessionID2Helper[sessionID];
+            var sequenceID = session.GetCurrentSequenceID();
+            session.StoreIncommingBytes(sequenceID, data);
+        }
+    }
+
+}
---
+++ BackendProxy2ASR/SessionHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendProxy2ASR
+{
+    class SessionHelper
+    {
+        public readonly string m_sessionID;
+        public readonly DateTime SesssionStartTime;
+        public Dictionary<int, string> m_sequence2uttID { get; set; }
+        public Dictionary<string, int> m_uttID2sequence { get; set; }
+        public Dictionary<int, string> m_sequence2inputword { get; set; }
+        public Dictionary<int, DateTime> m_sequenceStartTime { get; set; }
+        public Dictionary<int, List<byte>> m_sequenceBytes { get; set; }
+        public Queue<int> m_sequenceQueue { get; set; }
+
+        private int CurrentSequenceID;
+
+        public SessionHelper()
+        {
+            m_sessionID = CreateSessionID();
+            SesssionStartTime = DateTime.UtcNow;
+            m_sequence2uttID = new Dictionary<int, string>();
+            m_uttID2sequence = new Dictionary<string, int>();
+            m_sequence2inputword = new Dictionary<int, string>();
+            m_sequenceStartTime = new Dictionary<int, DateTime>();
+            m_sequenceBytes = new Dictionary<int, List<byte>>();
+            m_sequenceQueue = new Queue<int>();
+        }
+        public string CreateSessionID()
+        {
+            StringBuilder sessionID = new StringBuilder("");
+            Guid g = Guid.NewGuid();
+            //sessionID.Append(g);
+            sessionID.Append("90009");
+            return sessionID.ToString();
+        }
+
+        public int GetCurrentSequenceID()
+        {
+            if (m_sequenceQueue.Count != 0)
+            {
+                CurrentSequenceID = m_sequenceQueue.Dequeue();
+            }
+
+            return CurrentSequenceID;
+        }
+
+        public void StoreIncommingBytes(int seqenceID, byte[] data)
+        {
+            if (m_sequenceBytes.ContainsKey(seqenceID) == false)
+            {
+                m_sequenceBytes[seqenceID] = new List<byte>();
+            }
+            m_sequenceBytes[seqenceID].AddRange(data);
+        }
+
+        public byte[] RetrieveSequenceBytes(int seqenceID)
+        {
+            return m_sequenceBytes[seqenceID].ToArray();
+        }
+    }
+}
